522da4f28911e8300f915ea4ff6894066b9df6b3
Alexander Strole <astrole@student.42.us.org>
2018-03-19 05:32:25 -0700

    added stuffs

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
 	char		*d;
